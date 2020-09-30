8ba1f8508217df8b86cbc3991781c054e1b90cc3
JAI-2002 <72140256+JAI-2002@users.noreply.github.com>
2020-10-01 00:01:27 +0530

    New code will be easier to understand and is well written

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,23 +1,42 @@
-output = 0
-num1 = ""
-operation = ""
-num2 = ""
-num1 = input("Hello, What is your First Number?\n")
-operation = input("Operation (+, -, *, /)?\n")
-num2 = input("Your Second Number?\n")
+# output = 0
+# num1 = ""
+# operation = ""
+# num2 = ""
+# num1 = input("Hello, What is your First Number?\n")
+# operation = input("Operation (+, -, *, /)?\n")
+# num2 = input("Your Second Number?\n")
 
-floatnum1 = float(num1)
-floatnum2 = float(num2)
+# floatnum1 = float(num1)
+# floatnum2 = float(num2)
 
-if operation == "+":
-    output=floatnum1+floatnum2
-if operation == "-":
-    output=floatnum1-floatnum2
-if operation == "*":
-    output=floatnum1*floatnum2
-if operation == "/":
-    output=floatnum1/floatnum2
-if operation == "+" or operation == "-" or operation == "/" or operation == "*":
-    print("Your Answer: "+str(output))
+# if operation == "+":
+#     output=floatnum1+floatnum2
+# if operation == "-":
+#     output=floatnum1-floatnum2
+# if operation == "*":
+#     output=floatnum1*floatnum2
+# if operation == "/":
+#     output=floatnum1/floatnum2
+# if operation == "+" or operation == "-" or operation == "/" or operation == "*":
+#     print("Your Answer: "+str(output))
+# else:
+#     print("Your operation is invalid, please try again")
+
+
+
+
+# this new code is better i guess
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+operator = input("Enter an operator: ")
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
 else:
-    print("Your operation is invalid, please try again")
+    print("Invalid operator")
